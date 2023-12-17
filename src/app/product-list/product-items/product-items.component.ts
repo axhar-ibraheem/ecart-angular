0366c40914f4024722604d139e353b53e8f2c0a5
Azhar <azharibraheem482@gmail.com>
2023-12-17 11:48:46 +0530

    first commit

---
+++ src/app/product-list/product-items/product-items.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-items',
+  templateUrl: './product-items.component.html',
+  styleUrls: ['./product-items.component.css'],
+})
+export class ProductItemsComponent {
+  @Input()
+  product: {
+    id: number;
+    name: string;
+    description: string;
+    brand: string;
+    gender: string;
+    category: string;
+    size: number[];
+    color: string[];
+    price: number;
+    discountPrice?: number;
+    is_in_inventory: boolean;
+    items_left: number;
+    imageURL: string;
+    slug: string;
+  };
+}
